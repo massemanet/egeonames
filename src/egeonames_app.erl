d6ea72c5a66397cf38c0cb9a9b34cb9a97fd43a4
mats cronqvist <masse@klarna.com>
2014-01-20 11:11:35 +0100

    add rebar skeleton

---
+++ src/egeonames_app.erl
@@ -0,0 +1,16 @@
+-module(egeonames_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    egeonames_sup:start_link().
+
+stop(_State) ->
+    ok.
